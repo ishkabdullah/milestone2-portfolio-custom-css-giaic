e4e362f3c46d3d2327bd123252e0880d2bfbdaa8
ABDULLAH SHEIKH <137114130+ishkabdullah@users.noreply.github.com>
2024-12-09 00:43:15 +0500

    second commit

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -7,7 +7,7 @@ export default function Main() {
                 <h1 className="mainHeading">Salam! I am</h1>
                 <h1 className=" mainMainHeading"><span> Abdullah Sheikh</span> </h1>
                 <h2 className="mainHeadings">And I&apos;m a <span> Frontend Developer</span> </h2>
-                <p className="mainpara">Crafting the future with code and creativity &ndash; let's connect and innovate together..</p>
+                <p className="mainpara">Crafting the future with code and creativity &ndash; lets connect and innovate together..</p>
                 <a href="https://www.linkedin.com/in/ishkabdullah22/"><button className="leftButton">Hire me</button></a>
                 <a href="https://github.com/ishkabdullah"><button className="rightButton">See Projects</button></a>
             </div>
